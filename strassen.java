f36476c71a7823776cf5ff6850395bbd4d36aa97
Maegan Jong <maegan.jong@gmail.com>
2020-03-24 22:16:22 -0500

    edits

--- strassen.java
+++ strassen.java
@@ -3,6 +3,7 @@ import java.util.Random;
 import java.lang.Math;
 
 public class strassen{
+    const int CUTOFF = 30;
     public static void main (String[] args){
         // defines the command-line arguments
         // args[0] - 0, args[1] - dimension, args[2] - input file
@@ -26,7 +27,16 @@ public class strassen{
 
     public static int[][] strassen(int mat1[][], int mat2[][]){
         int N = mat1.length;
+
+        // implementing cut-off
+        if (N <= CUTOFF){
+          matrixMult(mat1[][], mat2[][]);
+        }
+
         int[][] res = new int[N][N];
+
+        // test whether N is odd
+        // if so, then figure out what where to pad
 
         int[][] a = new int[N / 2][N / 2];
         int[][] b = new int[N / 2][N / 2];
@@ -38,8 +48,25 @@ public class strassen{
         int[][] g = new int[N / 2][N / 2];
         int[][] h = new int[N / 2][N / 2];
 
+        int[][] p1 = strassen(addMatrix(a,d), addMatrix(e,d));
+        int[][] p2 = strassen(addMatrix(c,d),e);
+        int[][] p3 = strassen(a, subtractMatrix(f, h));
+        int[][] p4 = strassen(d, subtractMatrix(g, e));
+        int[][] p5 = strassen(addMatrix(a,b), h);
+        int[][] p6 = strassen(addMatrix(c,a), addMatrix(e,f));
+        int[][] p7 = strassen(subtractMatrix(b, d), addMatrix(g, h));
+
         return res;
     }
+
+    public static int[][] addMatrix(int mat1[][], int mat2[][]){
+
+    }
+
+    public static int[][] subtractMatrix(int mat1[][], int mat2[][]){
+
+    }
+
     public static int[][] matrixMult(int mat1[][], int mat2[][]){
         int i, j, k;
         int N = mat1.length;
@@ -50,8 +77,7 @@ public class strassen{
             {
                 res[i][j] = 0;
                 for (k = 0; k < N; k++)
-                    res[i][j] += mat1[i][k]
-                                * mat2[k][j];
+                    res[i][j] += mat1[i][k] * mat2[k][j];
             }
         }
 
